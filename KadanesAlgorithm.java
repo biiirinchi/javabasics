e32a72b80f7f284a9d2b476334aecea59e1e5dd5
Birinchi Chutia <biiirinchi09@gmail.com>
2025-09-10 23:18:09 +0530

    Create KadanesAlgorithm.java

---
+++ KadanesAlgorithm.java
@@ -0,0 +1,31 @@
+public class KadanesAlgorithm {
+    public static void maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > maxEndingHere + nums[i]) {
+                maxEndingHere = nums[i];
+                tempStart = i; 
+            } else {
+                maxEndingHere += nums[i]; 
+            }
+            if (maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+                start = tempStart;
+                end = i;
+            }
+        }
+        System.out.println("Maximum Subarray Sum: " + maxSoFar);
+        System.out.print("Subarray: [");
+        for (int i = start; i <= end; i++) {
+            System.out.print(nums[i]);
+            if (i < end) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        maxSubArray(arr);
+    }
+}
